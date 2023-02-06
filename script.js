df1a1c80a6ca49a9997f0d2019cad510a6fe14d0
Zwogli <Mathiaskohler@web.de>
2023-02-06 15:34:51 +0100

    add headline

--- script.js
+++ script.js
@@ -5,19 +5,8 @@ load();
 
 // ! in progress
 function render(){
+    getCurrentDate();
     let notes = document.getElementById('added-notes');
-    let newNotes = document.getElementById('new-notes');
-    newNotes.innerHTML = '';
-    newNotes.innerHTML = /*html*/`
-    <div id="new-note" class="note">
-            <input id="new-title" class="newNote-innerStyle" type="text" placeholder="Title" required>
-            <textarea  id="new-message" class="newNote-innerStyle" cols="10" rows="5" placeholder="Message" required></textarea>
-            <div class="add">
-                <img onclick="addNote()" id="add-icon" class="icon-note" src="img/icon/check-mark-2-64.png" alt="add Note">
-            </div>
-        </div>
-    `;
-
     notes.innerHTML = '';
     for (let i = 0; i < noteTitles.length; i++) {
         let title = noteTitles[i];
